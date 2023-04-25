ef42f1d044a547e4d71b4bcb6662231f794238ba
Jim Carlos Estenzo <jjlaverne1233@gmail.com>
2023-04-25 15:46:30 +0700

    Add NavBar

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import ColorScheme from './components/navbar.jsx'
+const App = () => {
+  return (
+    <>
+      {/* <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar> */}
+      <ColorScheme />
+      <h1>Hello World</h1>
+
+    </>
+  );
+}
+
+export default App;
